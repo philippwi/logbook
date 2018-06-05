d34323949c6bc603c7f7e1134f07aef57aef6b57
philippwi <philippwi95@gmail.com>
2018-06-05 21:44:12 +0200

    SQL file added

---
+++ sql/setupDatabase.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA `logbook` ;
+
+CREATE TABLE `logbook`.`user` (
+  `Name` VARCHAR(45) NOT NULL,
+  `Password` VARCHAR(45) NOT NULL,
+  `Admin` TINYINT NOT NULL,
+  PRIMARY KEY (`Name`));
+
+CREATE TABLE `logbook`.`trip` (
+  `TripID` INT NOT NULL AUTO_INCREMENT,
+  `User` VARCHAR(45) NOT NULL,
+  `Origin` VARCHAR(45) NOT NULL,
+  `Destination` VARCHAR(45) NOT NULL,
+  `Distance` FLOAT(6,1) NOT NULL,
+  `Date` DATE NOT NULL,
+  PRIMARY KEY (`TripID`));
